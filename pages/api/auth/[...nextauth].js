f37dd296504639b0b6bf4f3dd83ddb2fbd7e183d
DaeSeong_Hwang <co.la@kakao.com>
2022-02-08 02:47:11 +0900

    dotenv(only template)

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,18 +1,20 @@
 import NextAuth from 'next-auth'
 import KakaoProvider from 'next-auth/providers/kakao'
+import { KAKAO_CLIENT_ID, KAKAO_CLIENT_SECRET, NEXTAUTH_SECRET } from 'utill/dotenv'
 
 export default NextAuth({
+  debug: true,
+  secret: NEXTAUTH_SECRET,
   providers: [
     KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+      clientId: KAKAO_CLIENT_ID,
+      clientSecret: KAKAO_CLIENT_SECRET,
       authorization: {
-        url: `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_ID}`,
+        url: `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}`,
         params: { scope: 'profile_nickname', prompt: 'login' },
       },
     }),
   ],
-  debug: true,
   session: {
     maxAge: 60 * 60 * 24 * 7, // 7 Day
     updateAge: 60 * 60 * 12, // 12 H
